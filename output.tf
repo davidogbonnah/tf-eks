dfc8ab60861b9c9c3508372c2cbbf9893120e099
David Ogbonnah <davido6593@gmail.com>
2026-02-24 01:21:54 +0000

    Reduce scope of outputs

--- output.tf
+++ output.tf
@@ -1,5 +1,17 @@
 # Outputs could be limited to information required for authorization token, name and TLS Certificate
 
-output "eks_cluster" {
-    value = aws_eks_cluster.eks_cluster
+output "eks_cluster_name" {
+    value = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_cluster_endpoint" {
+    value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_tls_certificate" {
+    value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "eks_cluster_identity" {
+    value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
