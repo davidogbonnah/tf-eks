8ce1d9cf26a21fe551d6642b1fc51bbdf2a102e0
David Ogbonnah <davido6593@gmail.com>
2026-02-24 01:40:08 +0000

    Make output name more appropriate

--- output.tf
+++ output.tf
@@ -8,7 +8,7 @@ output "eks_cluster_endpoint" {
     value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "eks_tls_certificate" {
+output "certificate_authority_data" {
     value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
 
