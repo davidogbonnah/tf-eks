9b0b0e3617bac16ee8671a19e93d23c31939baa9
David Ogbonnah <davido6593@gmail.com>
2026-03-28 14:56:39 +0000

    Standardise Role Names 2

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ locals {
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.cluster_name}-role"
+  name = local.cluster_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
