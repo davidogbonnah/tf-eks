bd8fdc8d374cf7ee7dd5838402a7572052b3c8e2
David Ogbonnah <davido6593@gmail.com>
2026-03-30 23:59:23 +0100

    Add bootstrap_cluster_creator_admin_permissions

--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   encryption_config {
